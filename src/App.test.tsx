9e6a1ace11fe2fb22e1c5ebf30c18ef29ea59446
Emmanuel Oluyale <emmanueloluyale@Emmanuels-MacBook-Pro-2.local>
2021-09-19 19:53:04 +0100

    ref: clean up react app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/MCC/i);
   expect(linkElement).toBeInTheDocument();
 });
